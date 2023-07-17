9fe2d768011fd64205ef25f64cf7e9c9b230ff14
julien arino <julien.arino@gmail.com>
2023-07-17 01:13:33 -0500

    Update

--- plot-virtual-cohort.R
+++ plot-virtual-cohort.R
@@ -10,3 +10,12 @@ ggplot(df, aes(x = x, y = value, color = variable)) +
   theme(legend.position = "none")
 ggsave(filename = "FIGS/viral-load.png",
        width = 20, height = 15, units = "cm")
+
+df <- data.frame(x = STATE_VARS$time, SUMMARIES$V)
+df <- melt(df, id.vars = "x")
+ggplot(df, aes(x = x, y = value, color = variable)) +
+  geom_line() +
+  theme(legend.position = "none")
+ggsave(filename = "FIGS/summary-viral-load.png",
+       width = 20, height = 15, units = "cm")
+
--- process-virtual-cohort.R
+++ process-virtual-cohort.R
@@ -2,9 +2,27 @@ library(parallel)
 
 source("functions_all.R")
 
-t = COHORT[[1]][,"time"]
-V = mat.or.vec(nr = length(t), nc = N)
-for (i in 1:N) {
-  V[,i] = COHORT[[i]][,"V"]
+# Make a list with each entry the state variables. Assumes all 
+# individuals have the same time output and state variables
+STATE_VARS = list()
+for (n in colnames(COHORT[[1]])) {
+  if (n == "time") {
+    STATE_VARS[[n]] = COHORT[[1]][,"time"]
+  } else if ((n != "A_I") && (n != "A_R")) {
+    STATE_VARS[[n]] = mat.or.vec(nr = dim(COHORT[[1]])[1], 
+                                 nc = length(COHORT))
+    for (i in 1:length(COHORT)) {
+      STATE_VARS[[n]][,i] = COHORT[[i]][,n]
+    }
+  }
 }
 
+# Create summary view for state variables
+names_variables = setdiff(colnames(COHORT[[1]]), 
+                          c("time", "A_I", "A_R"))
+SUMMARIES = list()
+for (n in names_variables) {
+  SUMMARIES[[n]] = t(apply(STATE_VARS[[n]], 1, quantile))
+  SUMMARIES[[n]] = as.data.frame(SUMMARIES[[n]])
+  SUMMARIES[[n]]$mean = apply(STATE_VARS[[n]], 1, mean)
+}
