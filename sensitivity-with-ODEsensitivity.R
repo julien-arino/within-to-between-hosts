180f8764005ef50677e7d87948da30450e08845f
Julien Arino <julien.arino@gmail.com>
2023-11-26 10:33:27 -0600

    Update

--- sensitivity-with-ODEsensitivity.R
+++ sensitivity-with-ODEsensitivity.R
@@ -95,87 +95,55 @@ IC = set_IC()
 # The timepoints of interest:
 times <- c(0.01, seq(1, 50, by = 1))
 # Morris screening:
-set.seed(7292)
 # Warning: The following code might take very long!
-LVres_morris <- DDEmorris(mod = rhs_within_host_deSolve,
-                          pars = pars$params,
-                          state_init = IC,
-                          times = times,
-                          binf = pars$min,
-                          bsup = pars$max,
-                          r = 500,
-                          design = list(type = "oat",
-                                        levels = 10, grid.jump = 1),
-                          scale = TRUE,
-                          ode_method = "lsoda",
-                          parallel_eval = TRUE,
-                          parallel_eval_ncores = 6)
+writeLines("Starting DDEmorris")
+tictoc::tic()
+within_host_morris <- DDEmorris(mod = rhs_within_host_deSolve,
+                                pars = pars$params,
+                                state_init = IC,
+                                times = times,
+                                binf = pars$min,
+                                bsup = pars$max,
+                                r = 500,
+                                design = list(type = "oat",
+                                              levels = 10, grid.jump = 1),
+                                scale = TRUE,
+                                ode_method = "lsoda",
+                                parallel_eval = TRUE,
+                                parallel_eval_ncores = 60)
+tictoc::toc()
 
-set.seed(59281)
 # Sobol' sensitivity analysis (here only with n = 500, but n = 1000 is
 # recommended):
 # Warning: The following code might take very long!
-LVres_sobol <- ODEsobol(mod = LVmod,
-                        pars = LVpars,
-                        state_init = LVinit,
-                        times = LVtimes,
+writeLines("Starting DDEsobol")
+tictoc::tic()
+within_host_sobol <- DDEsobol(mod = rhs_within_host_deSolve,
+                        pars = pars$params,
+                        state_init = IC,
+                        times = times,
                         n = 500,
                         rfuncs = "runif",
-                        rargs = paste0("min = ", LVbinf,
-                                       ", max = ", LVbsup),
+                        rargs = paste0("min = ", pars$min,
+                                       ", max = ", pars$max),
                         sobol_method = "Martinez",
                         ode_method = "lsoda",
                         parallel_eval = TRUE,
-                        parallel_eval_ncores = 6)
+                        parallel_eval_ncores = 60)
+tictoc::toc()
 
-
-##### FitzHugh-Nagumo equations (Ramsay et al., 2007) #####
-FHNmod <- function(Time, State, Pars) {
-  with(as.list(c(State, Pars)), {
-    dVoltage <- s * (Voltage - Voltage^3 / 3 + Current)
-    dCurrent <- - 1 / s *(Voltage - a + b * Current)
-    return(list(c(dVoltage, dCurrent)))
-  })
-}
-# Warning: The following code might take very long!
-FHNres_morris <- ODEmorris(mod = FHNmod,
-                           pars = c("a", "b", "s"),
-                           state_init = c(Voltage = -1, Current = 1),
-                           times = seq(0.1, 50, by = 5),
-                           binf = c(0.18, 0.18, 2.8),
-                           bsup = c(0.22, 0.22, 3.2),
-                           r = 500,
-                           design = list(type = "oat",
-                                         levels = 50, grid.jump = 1),
-                           scale = TRUE,
-                           ode_method = "adams",
-                           parallel_eval = TRUE,
-                           parallel_eval_ncores = 6)
-  
-# Warning: The following code might take very long!
-
-FHNres_sobol <- ODEsobol(mod = FHNmod,
-                         pars = c("a", "b", "s"),
-                         state_init = c(Voltage = -1, Current = 1),
-                         times = seq(0.1, 50, by = 5),
-                         n = 500,
-                         rfuncs = "runif",
-                         rargs = c(rep("min = 0.18, max = 0.22", 2),
-                                   "min = 2.8, max = 3.2"),
-                         sobol_method = "Martinez",
-                         ode_method = "adams",
-                         parallel_eval = TRUE,
-                         parallel_eval_ncores = 6)
 
 # Just for demonstration purposes: The use of different distributions for the 
 # parameters (here, the distributions and their arguments are chosen 
 # completely arbitrarily):
 # Warning: The following code might take very long!
 
-demo_dists <- ODEsobol(mod = FHNmod,
-                       pars = c("a", "b", "s"),
-                       state_init = c(Voltage = -1, Current = 1),
-                       times = seq(0.1, 50, by = 5),
+writeLines("Starting DDEsobol with distributions")
+tictoc::tic()
+demo_dists <- DDEsobol(mod = rhs_within_host_deSolve,
+                       pars = pars$params,
+                       state_init = IC,
+                       times = times,
                        n = 500,
                        rfuncs = c("runif", "rnorm", "rexp"),
                        rargs = c("min = 0.18, max = 0.22",
@@ -184,4 +152,5 @@ demo_dists <- ODEsobol(mod = FHNmod,
                        sobol_method = "Martinez",
                        ode_method = "adams",
                        parallel_eval = TRUE,
-                       parallel_eval_ncores = 6)
+                       parallel_eval_ncores = 60)
+tictoc::toc()
