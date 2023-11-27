b947b71988d5503ec330b6ad2e7b61127c5f14d7
Julien Arino <julien.arino@gmail.com>
2023-11-27 09:06:09 -0600

    update

--- sensitivity-with-sensitivity.R
+++ sensitivity-with-sensitivity.R
@@ -45,6 +45,74 @@ rhs_within_host_deSolve = function(t, x, p) {
   })
 }
 
+# Given a patient index idx, a parameters data frame patients and 
+# initial conditions IC, run the simulation of the within host model for
+# this patient
+# Return only the indicators of that patient (V_max, etc.)
+run_one_patient_indicators = function(idx = 1, 
+                                      patients, 
+                                      IC) {
+  writeLines(paste0("patient index = ", idx))
+  params_tmp = patients[which(patients$ID == idx),]
+  params_tmp = add_IC_to_params(params_tmp, IC)
+  times <- c(seq(0, ceiling(params_tmp$tau_I), by = 0.01), 
+             seq(ceiling(params_tmp$tau_I), 200, by = 0.1))
+  tmp <- dede(y = IC, 
+               times = times, 
+               func = rhs_within_host_deSolve, 
+               parms = params_tmp)
+  OUT = list()
+  OUT$V_max = max(tmp[, "V"])
+  O
+  return(OUT)
+}
+
+# Create response for all computed indicators, returning a data frame for each
+# different point in parameter space provided as argument.
+value_indicators = function(params_change, 
+                            params_fixed, 
+                            t_f = 200,
+                            ncpus = 60,
+                            parallel = TRUE) {
+  writeLines("Creating list with parameters")
+  col_names = colnames(params_change)
+  PARS = list()
+  for (i in 1:dim(params_change)[1]) {
+    PARS[[i]] = list()
+    for (j in 1:dim(params_change)[2]) {
+      PARS[[i]][[col_names[j]]] = params_change[i,j]
+    }
+  }
+  writeLines("Setting up cluster")
+  if (parallel) {
+    # RUN IN PARALLEL
+    # Detect number of cores
+    no_cores <- detectCores()
+    if (no_cores > 124) {
+      # Detect rich person's problem. 
+      # (Could also recompile R setting the number of sockets higher than the 
+      # default... not done here.)
+      no_cores = 124
+    }
+    # Initiate cluster with parts shared across all batches
+    cl <- makeCluster(no_cores)
+    # Export needed variables
+    clusterEvalQ(cl,{
+      library(deSolve)
+    })
+    clusterExport(cl,
+                  c("run_one_patient",
+                    "add_IC_to_params",
+                    "rhs_within_host_deSolve",
+                    "IC"),
+                  envir = .GlobalEnv)
+  } else {
+    writeLines("We're running sequentially!")
+  }
+  writeLines("Starting simulations")
+  writeLines("Assembling results")
+}
+
 
 pars = data.frame(params = 
                     c("beta",
